89041b894a9d55a88cf9b8ef53f7e4e444eed473
Jeety BRIDJI <jeetylb@gmail.com>
2019-11-08 14:54:32 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoList';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDZR_kutmJgBz8rV5Bsl21IQGHHOd8oPe4",
+      authDomain: "todolist-2ba04.firebaseapp.com",
+      databaseURL: "https://todolist-2ba04.firebaseio.com",
+      projectId: "todolist-2ba04",
+      storageBucket: "todolist-2ba04.appspot.com",
+      messagingSenderId: "127415440809",
+      appId: "1:127415440809:web:6af24e58e29820b8a8a0b8"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
